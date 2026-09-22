cd1d2938a916d43e06a1ac3323c94032fbac2184
sshashanksingh4-max <sshashanksingh4@gmail.com>
2026-09-22 23:52:13 +0530

    feat: wire free-first media and YouTube bridge providers

--- app/services.py
+++ app/services.py
@@ -1,9 +1,12 @@
 from app.config import settings
 from app.providers.exa import ExaProvider
 from app.providers.higgsfield import HiggsfieldProvider
+from app.providers.pollinations import PollinationsProvider
 from app.providers.youtube import YouTubePublisher
+from app.providers.youtube_bridge import YouTubeBridgePublisher
 
 exa = ExaProvider(settings.exa_api_key)
+
 higgsfield = HiggsfieldProvider(
     settings.higgsfield_api_key_id,
     settings.higgsfield_api_key_secret,
@@ -11,8 +14,16 @@ higgsfield = HiggsfieldProvider(
     voice_id=settings.higgsfield_voice_id,
     voice_type=settings.higgsfield_voice_type,
 )
+
+pollinations = PollinationsProvider(settings.pollinations_api_key)
+
 youtube = YouTubePublisher(
     settings.youtube_client_id,
     settings.youtube_client_secret,
     settings.youtube_refresh_token,
+)
+
+youtube_bridge = YouTubeBridgePublisher(
+    settings.youtube_bridge_url,
+    settings.youtube_bridge_secret,
 )
