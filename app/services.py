faa1c1671c9b622e3a52eaaf8301a1156026bc6d
sshashanksingh4-max <sshashanksingh4@gmail.com>
2026-09-22 23:26:04 +0530

    Wire media provider configuration

--- app/services.py
+++ app/services.py
@@ -4,7 +4,13 @@ from app.providers.higgsfield import HiggsfieldProvider
 from app.providers.youtube import YouTubePublisher
 
 exa = ExaProvider(settings.exa_api_key)
-higgsfield = HiggsfieldProvider(settings.higgsfield_api_key_id, settings.higgsfield_api_key_secret)
+higgsfield = HiggsfieldProvider(
+    settings.higgsfield_api_key_id,
+    settings.higgsfield_api_key_secret,
+    video_model=settings.higgsfield_video_model,
+    voice_id=settings.higgsfield_voice_id,
+    voice_type=settings.higgsfield_voice_type,
+)
 youtube = YouTubePublisher(
     settings.youtube_client_id,
     settings.youtube_client_secret,
